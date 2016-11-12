5cd8708f8b1cede09a63197aece7f726897d33a5
icole <ian.cole@me.com>
2016-11-12 15:04:11 -0800

    Update itunes tmux track display

--- ansible/roles/tmux/files/itunes-current-track-tmux.sh
+++ ansible/roles/tmux/files/itunes-current-track-tmux.sh
@@ -18,5 +18,5 @@ on appIsRunning(appName)
 end appIsRunning
 EOF)
 
-    echo '#[bg=colour137,fg=colour16,bold] ♫' $ITUNES_TRACK '#[nobold]-#[bold]' $ITUNES_ARTIST '#[fg=colour254,bg=colour137,nobold]'
+    echo '#[fg=brightblue] ♫' $ITUNES_TRACK '-' $ITUNES_ARTIST
 fi
